9d4b3466e5285df066716a304f69f86b59c472cc
malogarzo <mariano.logarzo@gmail.com>
2018-09-08 12:23:51 -0300

    add css

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends React.Component {
 	render() {
+		// const styles = {
+		// 	container: {
+		// 		color: '#44546B',
+		// 		cursor: 'pointer',
+		// 		width: 260,
+		// 		border: '1px solid red'
+		// 	}
+		// }
 		return (
-			<div>
+			<div className="Media">
 				<div>
 					<img 
-						src="" 
+						src="./images/owilde.jpg" 
 						alt=""
 						width={260}
 						height={160}
 				 	/>
-				 	<h3>Frases que me gustan...</h3>
+				 	<h3>Quotes</h3>
 				 	<p>Mariano Logarzo</p>
 				</div>
 			</div>	

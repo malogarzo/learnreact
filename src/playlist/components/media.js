fb4f6606db8c39239d1b5c105e15f63c125fd2c2
malogarzo <mariano.logarzo@gmail.com>
2018-09-08 10:58:08 -0300

    add component

---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Media extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<img 
+						src="" 
+						alt=""
+						width={260}
+						height={160}
+				 	/>
+				 	<h3>Frases que me gustan...</h3>
+				 	<p>Mariano Logarzo</p>
+				</div>
+			</div>	
+		)
+	}
+}
+
+export default Media;

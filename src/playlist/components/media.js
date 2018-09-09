e6ae0152cd1dbd8f058c70ea4744af461fcdb8a8
malogarzo <mariano.logarzo@gmail.com>
2018-09-09 13:01:46 -0300

    typechecking propTypes

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends React.Component {
@@ -26,6 +27,14 @@ class Media extends React.Component {
 			</div>	
 		)
 	}
+}
+
+
+Media.propTypes = {
+	image: PropTypes.string,
+	title: PropTypes.string.isRequired,
+	author: PropTypes.string,
+	type: PropTypes.oneOf(['video', 'audio']),
 }
 
 export default Media;

61457b2588ea8a1395233dc42c04a2da28228100
malogarzo <mariano.logarzo@gmail.com>
2018-09-10 19:08:50 -0300

    events

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media.js'
 
 // console.log('Hola mundo');
 
 const app = document.getElementById('app');
 
 // ReactDOM.render(qué, donde);
-const helloWorld = <h1>Hello friend!</h1>;    
-ReactDOM.render(helloWorld , app);
+// const helloWorld = <h1>Hello friend!</h1>;    
+render(<Media type="video" title="Who is?" author="@malogarzo" image="./images/owilde.jpg"/> , app);
+
 
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,41 +3,41 @@ import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends React.Component {
-    handleClick(event) {
-        console.log()
-    }
-    // render() {
-        // const styles = {
-        // 	container: {
-        // 		color: '#44546B',
-        // 		cursor: 'pointer',
-        // 		width: 260,
-        // 		border: '1px solid red'
-        // 	}
-        // }
-        return (
-            <div className="Media" onClick={this.handleClick}>
-                <div className="Media-cover">
-                    <img 
-                        src={this.props.image} 
-                        alt=""
-                        width={260}
-                        height={160}
-                     />
-                     <h3 className="Media-title">{this.props.title}</h3>
-                     <p className="Media-author">{this.props.author}</p>
-                </div>
-            </div>	
-        )
-    }
+  handleClick = (event) => {
+    console.log(this.props.image)
+  }
+  render() {
+      const styles = {
+      	container: {
+      		color: '#44546B',
+      		cursor: 'pointer',
+      		width: 260,
+      		border: '1px solid red'
+      	}
+      }
+      return (
+        <div className="Media" onClick={this.handleClick}>
+          <div className="Media-cover">
+            <img 
+              src={this.props.image} 
+              alt=""
+              width={260}
+              height={160}
+             />
+             <h3 className="Media-title">{this.props.title}</h3>
+             <p className="Media-author">{this.props.author}</p>
+          </div>
+        </div>	
+    )
+  }
 }
 
 
 Media.propTypes = {
-    image: PropTypes.string,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string,
-    type: PropTypes.oneOf(['video', 'audio']),
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video', 'audio']),
 }
 
 export default Media;

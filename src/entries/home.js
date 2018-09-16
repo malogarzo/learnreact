af2dbff66cad32aa6d30ef52fdfc4ea9fe0b47ec
malogarzo <mariano.logarzo@gmail.com>
2018-09-16 20:47:47 -0300

    add categories

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Home from '../pages/containers/home.js';
+import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist.js';
-// import data from './src/api.json';
+import data from '../api.json';
 
 // console.log('Hola mundo');
 
@@ -10,4 +10,4 @@ const app = document.getElementById('app');
 
 // ReactDOM.render(qué, donde);
 // const helloWorld = <h1>Hello friend!</h1>;    
-render( <Home /> , app);
+render( <Home data={data} />, app);
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import Media from './media.js';
 // import './playlist.css';
-import Play from '../../icons/components/play';
 
 function Playlist(props) {
-  const playlist = props.data.categories[0].playlist
-  console.log(props.data);
   return (
     <div className="Playlist">
-      <Play 
-        size={50}
-        color="red"
-      />
       {
-        playlist.map((item) => {
+        props.playlist.map((item) => {
           return <Media {...item} key={item.id} />
         })
       }

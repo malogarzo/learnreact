1f3f811ca8260f59c2d727fa77f63ab6bff98260
malogarzo <mariano.logarzo@gmail.com>
2018-09-17 19:39:25 -0300

    add css components

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout.js';
 import Categories from '../../categories/components/categories';
+import Related from '../components/related.js';
 
 class Home extends Component {
 	render() {
 		return (
 			<HomeLayout>
+				<Related />
 				<Categories categories={this.props.data.categories} />
 			</HomeLayout>
 		)

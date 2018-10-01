8486074d0e7824b56b62fe1d554add8d13b57afb
malogarzo <mariano.logarzo@gmail.com>
2018-10-01 20:07:33 -0300

    add play/pause control

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout.js'
 import Video from '../components/video.js';
 import Title from '../components/title.js';
+import PlayPause from '../components/play-pause.js';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
+  componentDidMount() {
+    this.setState({
+      pause: (!this.props.autoplay)
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
           title="This is a video"
         />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
+        />
         <Video
-          autoplay={true}
+          autoplay={this.props.autoplay}
+          pause={this.state.pause}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>

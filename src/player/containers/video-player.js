c8c8be9844b5e560c2657b31939b5263a85cf6de
malogarzo <mariano.logarzo@gmail.com>
2018-10-01 20:10:04 -0300

    video-player

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout.js'
 import Video from '../components/video.js';
 import Title from '../components/title.js';
-import PlayPause from '../components/play-pause.js';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
   state = {

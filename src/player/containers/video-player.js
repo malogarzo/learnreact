7ff348e2d204d8dbe733f5c4341bd7ce177eaecd
malogarzo <mariano.logarzo@gmail.com>
2018-10-06 10:55:22 -0300

    set video current time

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -9,7 +9,8 @@ import Controls from '../components/video-player-controls.js';
 class VideoPlayer extends Component {
   state = {
     pause: true,
-    duration: 0
+    duration: 0,
+    currentTime: 0,
   }
   togglePlay = (event) => {
     this.setState({
@@ -27,11 +28,17 @@ class VideoPlayer extends Component {
       duration: this.video.duration
     });
   }
+  handleTimeUpdate = event => {
+    console.log(this.video.currentTime)
+    this.setState({
+      currentTime: this.video.currentTime
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
-          title="This is a video"
+          title="Video"
         />
         <Controls>
           <PlayPause
@@ -40,12 +47,14 @@ class VideoPlayer extends Component {
           />
           <Timer 
             duration={this.state.duration}
-          />
+            currentTime={this.state.currentTime}
+          />  
         </Controls>
         <Video
           autoplay={this.props.autoplay}
           pause={this.state.pause}
           handleLoadedMetadata={this.handleLoadedMetadata}
+          handleTimeUpdate={this.handleTimeUpdate}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>

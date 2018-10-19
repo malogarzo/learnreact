518d041c67e4ec54ab0a336e8b6d8a6e3cfb435c
malogarzo <mariano.logarzo@gmail.com>
2018-10-18 21:37:37 -0300

    fix spinner

---
+++ src/player/components/fullscreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FullScreenIcon from '../../icons/components/fullscreen.js';
+import './fullscreen.css';
+
+const FullScreen = (props) => (
+  <div 
+    className="Fullscreen"
+    onClick={props.handleFullScreenClick}
+  >
+    <FullScreenIcon
+      size={25}
+      color="white"
+    />
+  </div>  
+)
+
+export default FullScreen;
--- src/player/components/spinner.js
+++ src/player/components/spinner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './spinner.css';
 
 function Spinner(props) {
+  if (!props.active) return null
   return (
     <div className="Spinner">
       <span>Cargando...</span>
